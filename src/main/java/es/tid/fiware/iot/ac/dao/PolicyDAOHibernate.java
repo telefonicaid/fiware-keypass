acf7f2a9a36970f427cd433ac6ffdea62900dd87
Daniel Moran <dmj@tid.es>
2014-09-19 14:32:14 +0200

    ADD Liquibase migration scripts

---
+++ src/main/java/es/tid/fiware/iot/ac/dao/PolicyDAOHibernate.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.tid.fiware.iot.ac.dao;
+
+import es.tid.fiware.iot.ac.model.Policy;
+import io.dropwizard.hibernate.AbstractDAO;
+import java.util.Collection;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+public class PolicyDAOHibernate extends AbstractDAO<Policy> implements PolicyDao  {
+
+    public PolicyDAOHibernate(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public Policy createPolicy(Policy policy) {
+        return persist(policy);
+    }
+
+    @Override
+    public Policy loadPolicy(String tenant, String subject, String id) {
+        return get(new Policy.PolicyId(tenant, id));
+    }
+
+    @Override
+    public Collection<Policy> getPolicies(String tenant, String subject) {        
+        Criteria ct = this.currentSession().createCriteria(Policy.class)
+            .add( Restrictions.eq("internalId.tenant", tenant) )
+            .add( Restrictions.eq("subject", subject));
+        return list(ct);
+    }
+
+    @Override
+    public Collection<String> getSubjects(String tenant) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Policy updatePolicy(Policy policy) {
+        String hql = "UPDATE Policy SET policy = :policy "  + 
+             "WHERE tenant = :tenant and internalId.id = :id";
+        
+        Query query = this.currentSession().createQuery(hql);
+        query.setParameter("policy", policy.getPolicy());
+        query.setParameter("tenant", policy.getInternalId().getTenant());
+        query.setParameter("id", policy.getInternalId().getId());
+        int result = query.executeUpdate();
+
+        System.out.println("The result of the update is " + result); 
+        
+        return policy;
+    }
+
+    @Override
+    public Policy deletePolicy(Policy policy) {
+        this.currentSession().delete(policy);
+        
+        return policy;
+    }
+
+    @Override
+    public void deleteFromTenant(String tenant) {
+        String hql = "DELETE FROM Policy "  + 
+             "WHERE internalId.tenant = :tenant_id";
+        
+        Query query = this.currentSession().createQuery(hql);
+        query.setParameter("tenant_id", tenant);
+        int result = query.executeUpdate();
+        
+        System.out.println("The result of the delete tenant is " + result);
+    }
+
+    @Override
+    public void deleteFromSubject(String tenant, String subject) {
+        String hql = "DELETE FROM Policy "  + 
+             "WHERE subject = :subject_id";
+        
+        Query query = this.currentSession().createQuery(hql);
+        query.setParameter("subject_id", subject);
+        int result = query.executeUpdate();
+
+        System.out.println("The result of the delete subject is " + result);        
+    }
+    
+}
