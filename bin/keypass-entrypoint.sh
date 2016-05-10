2c54c3b6548527edb951f45fb4d721d671237c43
Alvaro Vega <avega@tid.es>
2016-05-10 10:07:39 +0200

    add sleep before config db

--- bin/keypass-entrypoint.sh
+++ bin/keypass-entrypoint.sh
@@ -6,6 +6,7 @@ DB_HOST_VALUE=${2}
 
 if [ "$DB_HOST_ARG" == "-dbhost" ]; then
     sed -i "s/mysql:\/\/localhost/mysql:\/\/"$DB_HOST_VALUE"/g" /opt/keypass/config.yml
+    sleep 30
     java -jar /opt/keypass/keypass.jar db migrate /opt/keypass/config.yml
 fi
 
